f2d9da20231455fe6038c52c6025f5529a9cf1f5
markov-eni <marc.delefosse2025@campus-eni.fr>
2026-01-20 13:46:11 +0100

    ADD e2e tests to GET & POST /hello

--- tests/e2e/e2e.test.js
+++ tests/e2e/e2e.test.js
@@ -21,4 +21,84 @@ describe("E2E GET /hello", () => {
     expect(res.status).toBe(200);
     expect(res.data).toBe("Hello world!");
   });
+
+  it("responds with Hello world! From {name}", async () => {
+    const name = "Alice";
+    const res = await axios.get(`${baseURL}/hello/${name}`);
+    expect(res.status).toBe(200);
+    expect(res.data).toBe(`Hello world! From ${name}`);
+  });
+
+  it("responds with 404 if it contains backslash", async () => {
+    const name = "Alice/Bob";
+  
+    await expect(
+      axios.get(`${baseURL}/hello/${name}`)
+    ).rejects.toMatchObject({
+      response: { status: 404 },
+    });
+  });
+  
+  it("responds with 400 when name contains forbidden characters", async () => {
+    const name = "Alice<script>";
+  
+    await expect(
+      axios.get(`${baseURL}/hello/${name}`)
+    ).rejects.toMatchObject({
+      response: { status: 400 },
+    });
+  });
+  
+  it("responds with 400 when name is too long", async () => {
+    const longName = "a".repeat(101);
+  
+    await expect(
+      axios.get(`${baseURL}/hello/${longName}`)
+    ).rejects.toMatchObject({
+      response: { status: 400 },
+    });
+  });
+});
+
+describe("E2E POST /hello", () => {
+  it("responds with Hello world", async () => {
+    const res = await axios.post(`${baseURL}/hello`);
+    expect(res.status).toBe(200);
+    expect(res.data).toBe("Hello world!");
+  });
+
+  it("responds with Hello world! From {x-name}", async () => {
+    const name = "Bob";
+    const res = await axios.post(`${baseURL}/hello`, {}, {
+      headers: { "x-name": name }
+    });
+    expect(res.status).toBe(200);
+    expect(res.data).toBe(`Hello world! From ${name}`);
+  });
+
+
+  it("responds with 400 when name contains forbidden characters", async () => {
+    const name = "Alice<script>";
+  
+    await expect(
+      axios.post(`${baseURL}/hello`, {}, {
+        headers: { "x-name": name }
+      })
+    ).rejects.toMatchObject({
+      response: { status: 400 },
+    });
+  });
+
+  it("responds with 400 when name is too long", async () => {
+    const longName = "a".repeat(101);
+  
+    await expect(
+      axios.post(`${baseURL}/hello`, {}, {
+        headers: { "x-name": longName }
+      })
+    ).rejects.toMatchObject({
+      response: { status: 400 },
+    });
+  });
+
 });
